a8746ba51cb521ea8c5f1d4f0cf22ceabf6a5f4a
Girverius <gsmac1224@gmail.com>
2021-03-02 22:44:17 -0600

    added app.js and index

---
+++ index.js
@@ -0,0 +1,7 @@
+import reactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDom.render(<App />, document.getElementById('root'));
