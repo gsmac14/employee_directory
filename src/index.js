fcbe93151bce877eb39db783e62eb80150355736
Girverius <gsmac1224@gmail.com>
2021-03-04 22:35:20 -0600

    added header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import reactDom from "react-dom";
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
